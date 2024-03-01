6724fa254f160d3625b0f9ed0bcabfd49fa96b84
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-03-01 11:27:49 +0530

    Unique elments in an array done

--- 3-Arrays/RemoveDup.cpp
+++ 3-Arrays/RemoveDup.cpp
@@ -2,29 +2,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void PrintArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+void PrintArray(int arr[], int n)
+{
+    cout<<"The array is : "<<endl;
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-void TakeInput(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << "Enter the " << i + 1 << "th element of array: ";
-        cin >> arr[i];
+void TakeInput(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            cout << "Enter the " << i + 1 << "st element of array: ";
+            cin >> arr[i];
+        }
+        else if (i == 1)
+        {
+            cout << "Enter the " << i + 1 << "nd element of array: ";
+            cin >> arr[i];
+        }
+         else if (i == 2)
+        {
+            cout << "Enter the " << i + 1 << "rd element of array: ";
+            cin >> arr[i];
+        }
+         else 
+        {
+            cout << "Enter the " << i + 1 << "th element of array: ";
+            cin >> arr[i];
+        }
     }
-    cout << endl;
+}
+int RemoveDup(int arr[],int n){
+    int i = 0;
+    for(int j=1;j<n;j++){
+        if(arr[i]!=arr[j]){
+            arr[i+1]=arr[j];
+            i++;
+        }
+    }
+    return i+1;
 }
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter the size of array: ";
     cin >> n;
 
-    int arr[n]; 
+    int arr[n];
 
     TakeInput(arr, n);
-
+    PrintArray(arr, n);
+    int ind = RemoveDup(arr,n);
+    cout<<"Number of unique elements in the array : "<<ind<<endl;
     return 0;
 }
