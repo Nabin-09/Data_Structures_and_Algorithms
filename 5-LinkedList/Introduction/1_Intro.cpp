eb80be9328ff288d961a11742820f046ee067a57
Nabin <sharmanabin.0910@gmail.com>
2024-06-14 21:22:45 +0530

    LinkedLists- Intro

--- 5-LinkedList/Introduction/1_Intro.cpp
+++ 5-LinkedList/Introduction/1_Intro.cpp
@@ -1,21 +1,27 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 struct Node{
-    public:
     int data;
     Node* next;
-    public: 
-
-    Node(int data1,Node* next1){//Constructo
+    Node(int data1,Node* next1){
         data = data1;
         next = next1;
     }
 };
 int main()
 {
-    std::vector<int> arr = {2,3,4,5,6,7};
-    Node* y = new Node(arr[0],nullptr);
-    cout << y; //0x716f28 Prints pointer to the memory location (New provides it)
+    Node x = Node(2,nullptr); // Initialising just like we initialise integers or float
+    Node* y = &x; // getting its memory address to use further
+    /*Better than using above syntax we can use : */
+    Node* m = new Node(3,nullptr); // m stores a pointer to the memory location 
+    cout<<m; //0x736f08
+    /*
+    Node z = Node(3,nullptr)
+    cout<<z Throws error how can we even print an Object
+    we can print z.data and z.next 
+    and if we do 
+    Node* y = new Node(2,nullptr)
+    we cannot use y.data and y.next we must use y->data and y->next
+    */
     return 0;
 }
