dc33c9f2eb32886e4603679b0f1cca2b9478157f
Nabin <sharmanabin.0910@gmail.com>
2024-06-15 20:21:19 +0530

    Length Of LinkedLists

---
+++ 5-LinkedList/Introduction/4_Length.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+Node* ConvertToLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 0;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;//mover =  mover -> next (equivalent)
+    }
+    return head;
+}
+int LengthofLL(Node* head){
+    int cnt = 0;
+    Node* temp = head;
+    while(temp){
+        temp = temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+int main()
+{
+    vector<int> arr = {1,2,3,4,5,6,7};
+    Node* head = ConvertToLL(arr);
+    cout<<LengthofLL(head);
+    return 0;
+}
