81b6794549eda52c6be95d45563452da122f165d
Nabin <sharmanabin.0910@gmail.com>
2024-06-22 11:52:34 +0530

    Delete Tail of DLL

--- 5-LinkedList/Introduction/Doubly-Linked-List/3_DeleteTail.cpp
+++ 5-LinkedList/Introduction/Doubly-Linked-List/3_DeleteTail.cpp
@@ -23,7 +23,7 @@ Node* ConvertToDLL(vector<int> arr){
     Node* prev = head;
     for(int i = 1;i<arr.size();i++){
         Node* temp = new Node (arr[i]);
-        prev->next = temp ;
+        prev->next = temp ; 
         prev = temp;
     }
     return head;
@@ -35,9 +35,40 @@ void print(Node* head){
         temp = temp -> next;
     }
 }
+Node* DeleteTail(Node* head) {
+    if (head == NULL || head->next == NULL) {
+        // If the list is empty or has only one node, return NULL
+        if (head != NULL) {
+            // If there's a single node, free it
+            delete head;  // Assuming nodes are allocated with 'new'
+        }
+        return NULL;
+    }
+
+    // Traverse to the second-to-last node
+    Node* tail = head;
+    while (tail->next->next != NULL) {
+        tail = tail->next;
+    }
+
+    // Now 'tail' points to the second-to-last node
+    Node* lastNode = tail->next;  // The node to delete
+
+    // Update the second-to-last node to be the new tail
+    tail->next = NULL;
+
+    // Free the memory of the last node
+    delete lastNode;  // Assuming nodes are allocated with 'new'
+
+    // Return the head of the list
+    return head;
+}
 int main()
 {
     //23:37
-    
+    vector<int> arr = {1,2,3,4,5,6};
+    Node* head = ConvertToDLL(arr);
+    head = DeleteTail(head);
+    print(head);
     return 0;
 }
