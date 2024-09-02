7818f26c2c0ac6e2c1f2866d3b8ffaa194dfe23c
Nabin <sharmanabin.0910@gmail.com>
2024-09-02 19:47:47 +0530

    LinkedLists added

---
+++ 5-LinkedList/Introduction/Advanced-Problems/Y-LinkedList.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int data1 , Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+int main()
+{
+    
+    return 0;
+}
