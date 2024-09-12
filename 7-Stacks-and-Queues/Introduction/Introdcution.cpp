0da1fbe78cc370a605322d659a8bfbc9299a5604
Nabin <sharmanabin.0910@gmail.com>
2024-09-12 22:44:35 +0530

    Stacks-Intro

---
+++ 7-Stacks-and-Queues/Introduction/Introdcution.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+int main()
+{
+    /*We will be going through what is stack and queue
+    and their impelentation using 
+    Arrays
+    LinkedList
+    and queues using stack
+    Stack using queues
+    What is Stack?  It is data Structure that holds a certain type of data and 
+    follows LIFO Mechanism , Last IN First OUT.
+    We need to know size of stack before using it 
+    take a variable top and assign it -1
+    push means pushing a value in stack
+    pop means popping out a value from stack
+    move the top to the latest value that entered the stack*/
+    
+    return 0;
+}
