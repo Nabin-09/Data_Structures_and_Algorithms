31ff18f7e6a6a6de194e361b370d82a4e0bdf5e1
Nabin <sharmanabin.0910@gmail.com>
2024-11-01 21:44:19 +0530

    Weighted Graph using 2D array

--- 8-Graphs/Introduction/3_ArrWeighted.cpp
+++ 8-Graphs/Introduction/3_ArrWeighted.cpp
@@ -5,6 +5,14 @@ using namespace std;
 int main() {
     /*Every this is same , the only difference is we will be marking the spot with Weight of edge and not 1*/
     int n , m;
-    int adj[n+1][n+1]         
+    cin >> n >> m;
+    int adj[n+1][n+1]; 
+    for(int i = 0 ; i < m ; i++){
+        int u , v , weight;
+        cin >> u >> v >> weight;
+        adj[u][v] = weight;
+        adj[v][u] = weight;
+    }
+    /*Very simple Implementation*/    
     return 0;
 }
