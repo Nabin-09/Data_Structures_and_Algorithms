9d8a2b17054ec2d18e08edd08a624607069f66a7
Nabin <sharmanabin.0910@gmail.com>
2025-02-05 23:46:39 +0530

    Making a tree for traversal

---
+++ 10-Trees/2_PreOrderTraversal.cpp
@@ -0,0 +1,25 @@
+//Root Left Right
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->right = new Node(5);
+    root -> left -> left = new Node(4);
+    root->left->right->left = new Node(6);
+    root->right = new Node(3);
+    root->right->left = new Node(7);
+    root->right->right = new Node(8);
+    root->right->right->left = new Node(9);
+    root->right->right->right  = new Node(10);
+}
