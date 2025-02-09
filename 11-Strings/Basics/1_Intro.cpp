7dbe35857ed2dd79ed88aad50489df429cd23cf4
Nabin <sharmanabin.0910@gmail.com>
2025-02-09 13:17:36 +0530

    String vs String Literal

---
+++ 11-Strings/Basics/1_Intro.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+const int MOD = 10e7 +7;
+const int INF = LLONG_MAX >> 1;
+
+signed main(){
+    string s = "abcde";//set of characters , the one on left is string and on left is string literal
+    //string literals = set of charcters but cant be edit
+    auto i = 123.89;//gives data types by self to the variable
+}
