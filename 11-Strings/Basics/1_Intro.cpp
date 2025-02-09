4ac1d9c12289dd2af853125624cff8ead3413420
Nabin <sharmanabin.0910@gmail.com>
2025-02-09 13:35:09 +0530

    + and iterators

--- 11-Strings/Basics/1_Intro.cpp
+++ 11-Strings/Basics/1_Intro.cpp
@@ -10,4 +10,13 @@ signed main(){
     string s = "abcde";//set of characters , the one on left is string and on left is string literal
     //string literals = set of charcters but cant be edit
     auto i = 123.89;//gives data types by self to the variable
+    s[1] = 'z';//we can change the characters at index but cant do s[1] = "Fe"  
+    //Why string is used over vector?
+    //vector<char> = {a , b ,c } cout<<v throws error
+    cout<<s<<endl;//works , we can push back , reverse convert t UpCase and LoCase , and many more functions
+    //the '+' operator concats strings
+    //s.begin() return the index 0 and s.end() return the index after the last element
+    reverse(s.begin() , s.end());//s.end() points outside the array
+    cout<<s<<endl;//returns edcza 
+
 }
