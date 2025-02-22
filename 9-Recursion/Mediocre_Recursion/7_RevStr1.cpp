e51f24c19d59d2a8dfe9a579ebd74a7ff64558b6
Nabin <sharmanabin.0910@gmail.com>
2025-02-22 13:57:41 +0530

    Recursion approach 2

---
+++ 9-Recursion/Mediocre_Recursion/7_RevStr1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(string s , int idx){
+    if(idx >= s.size()){
+        return;
+    }
+    solve(s, idx+1);
+    cout<<s[idx];
+}
+int main() {
+    string s;
+    cin >> s;
+    solve(s, 0);
+    return 0;
+}
