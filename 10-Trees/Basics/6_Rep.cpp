0772c91c480351af8fb23c8383e6793175312c1c
Nabin <sharmanabin.0910@gmail.com>
2025-04-18 21:51:08 +0530

    Representation of BT

---
+++ 10-Trees/Basics/6_Rep.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }   
+};
+int main() {
+
+    return 0;
+}
