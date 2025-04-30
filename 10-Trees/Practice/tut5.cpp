5bf42154a318ff21b8341e1c14de5b75938faf51
Nabin <sharmanabin.0910@gmail.com>
2025-04-30 11:37:20 +0530

    Root to Node

--- 10-Trees/Practice/tut5.cpp
+++ 10-Trees/Practice/tut5.cpp
@@ -1,1 +1,36 @@
-//Level Order traversal - BFS
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+bool getPath(Node* root , vector<int> &arr , int x ){
+    if(!root) return false;
+    arr.push_back(root->data);
+
+    if(root -> data){
+        return true;
+    }
+    if(getPath(root->left , arr , x)||
+    getPath(root->right , arr , x)) return true;
+
+    arr.pop_back();
+    return false;
+}
+vector<int> RootToNode(Node* root , int target){
+    vector<int> arr;
+    if(root == NULL) return arr;
+    getPath(root , arr , target);
+    return arr;
+}
+int main() {
+    
+    return 0;
+}
