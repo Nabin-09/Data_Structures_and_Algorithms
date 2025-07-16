09efbd6b67e07ae39662d9612f692ba7092ccaa8
Nabin <sharmanabin.0910@gmail.com>
2025-07-16 13:21:45 +0530

    Reversing order

--- 9-Recursion/10dayChallenge/Recursion.cpp
+++ 9-Recursion/10dayChallenge/Recursion.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-void print(int i , int n ){
-    if (i == n) return;
-    cout << i << " ";
-    print(++i , n);
-}
+// void print(int i , int n ){
+//     if (i == n) return;
+//     cout << i << " ";
+//     print(++i , n);
+// }
 int main(){
     int n ;
     cin >> n;
-    print(1 ,n);
+    // print(1 ,n);
 }
