58d82039d8d20daa7f0538cb9c1805e3a174a672
Nabin <sharmanabin.0910@gmail.com>
2025-07-22 13:48:48 +0530

    Linear Search

--- 9-Recursion/10dayChallenge/LinearSearch.cpp
+++ 9-Recursion/10dayChallenge/LinearSearch.cpp
@@ -2,9 +2,21 @@
 
 using namespace std;
 
+bool LinearSearch(int *arr , int size , int k ){
+    if(size == 0) return false;
+    if(arr[0] == k) return true;
+
+    else{
+        bool rem = LinearSearch(arr+1, size-1 , k);
+        return rem;
+    }
+}
+
 int main(){
     int arr[] = {1 , 2 , 3 , 4 , 5 , 6 , 12};
     int size = 7;
-    int key = 4;
-    
+    int key = 45;
+
+
+    (LinearSearch(arr , size , key))? cout<<"FOUND" : cout <<"NOT FOUND";
 }
